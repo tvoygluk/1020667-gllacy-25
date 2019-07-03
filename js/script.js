d743f070401d063e231b2b79cd24b688bc9182df
Maxim Medyanik <tvoygluk@gmail.com>
2019-07-03 11:15:03 +0300

    TODO: форма регистрации

--- js/script.js
+++ js/script.js
@@ -1,34 +1,28 @@
-var openCatalogList = document.querySelector(".catalog-link");
-var catalogList = document.querySelector(".catalog-list");
-// var catalogLink = document.querySelectorAll(".close-catalog-list"), index, a;
+// var openCatalogList = document.querySelector(".catalog-link");
+// var catalogList = document.querySelector(".catalog-list");
 
-const addClassCatalogList = () => {
-  catalogList.classList.add("make-visible");
-}
 
-const removeClassCatalogList = () => {
-  catalogList.classList.remove("make-visible");
-}
-
-openCatalogList.addEventListener("click", evt => {
-  evt.preventDefault();
-  addClassCatalogList();
-});
-
-// for (index = 0; index < catalogLink.length; index++) {
-//   a = catalogLink[index];
-//   a.addEventListener('click', () => {
-//     removeClassCatalogList();
-//   });
+// const addClassCatalogList = () => {
+//   catalogList.classList.add("make-visible");
 // }
 
-document.addEventListener("click", evt => {
-  let target = evt.target;
-  let its_catalogList = target == catalogList || catalogList.contains(target);
-  let its_openCatalogList = target == openCatalogList;
-  let catalogList_is_active = catalogList.classList.contains("make-visible");
+// const removeClassCatalogList = () => {
+//   catalogList.classList.remove("make-visible");
+// }
 
-  if (!its_catalogList && !its_openCatalogList && catalogList_is_active) {
-    removeClassCatalogList();
-  }
-});
+// openCatalogList.addEventListener("click", evt => {
+//   evt.preventDefault();
+//   addClassCatalogList();
+// });
+
+
+// document.addEventListener("click", evt => {
+//   let target = evt.target;
+//   let its_catalogList = target == catalogList || catalogList.contains(target);
+//   let its_openCatalogList = target == openCatalogList;
+//   let catalogList_is_active = catalogList.classList.contains("make-visible");
+
+//   if (!its_catalogList && !its_openCatalogList && catalogList_is_active) {
+//     removeClassCatalogList();
+//   }
+// });
